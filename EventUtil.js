192127b8c137ea0c4b0541d99976302723847c2a
juncheng1 <494208562@qq.com>
2017-04-01 10:48:08 +0800

    getRelatedTarget
    add function getRelatedTarget

--- EventUtil.js
+++ EventUtil.js
@@ -37,6 +37,17 @@
 			}else{
 				event.cancelBubble = true;
 			}
+		},
+		getRelatedTarget:function(event){
+			if(event.relatedTarget){
+				return event.relatedTarget;
+			}else if(event.toElement){
+				return event.toElement;
+			}else if(event.fromElement){
+				return event.fromElement;
+			}else{
+				return null;
+			}
 		}
 	}
 })();
